ca304e70c79954835b3c2ccf11d6c38d15360267
Matthew Shultz <matthew.shultz@yale.edu>
2014-10-29 21:07:02 -0400

    Rename stream_order script and start on assign elevation script

---
+++ assign_elevations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import psycopg2
+
+junctions_table = "hubbard_net_junctions"
+raster_table = "hubbard_watershed_dem"
+
+conn = psycopg2.connect(database="streams", password="", host="127.0.0.1", port="5432")
+cur = conn.cursor()
+
+update = "update %s " % (junctions_table) + " junctions " \
+					"SET elevation = ST_VALUE(rast, junctions.geom) " \
+					"FROM  \"%s\" junctions2 CROSS JOIN \"%s\" raster " % (junctions_table, raster_table) + " " \
+					"WHERE junctions.gid = junctions2.gid"
+print update
+
